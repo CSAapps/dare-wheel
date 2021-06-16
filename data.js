002cbbffc2a7f12839a56941abb3a480dec12540
CSA <abeywickramakcs.19@uom.lk>
2021-06-16 15:15:34 +0530

    add data.js

---
+++ data.js
@@ -0,0 +1,31 @@
+var dares = [
+    "dare 1",
+    "dare 2",
+    "dare 3",
+    "dare 4",
+    "dare 5",
+    "dare 6",
+    "dare 7",
+    "dare 8",
+    "dare 9",
+    "dare 10"
+];
+
+var colors = [
+    "#302579",
+    "FF007B",
+    "CB22D7",
+    "3D5AF1",
+    "EC823A",
+    "E45171",
+    "00B906",
+    "F21368",
+    "FF9A00",
+    "3A0077",
+    "A34A28",
+    "E85395",
+    "A73CCB",
+    "0278AE",
+    "FA9905",
+    "FF5200",
+]
--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+function e(id) {
+    return document.getElementById(id);
+}
+var segs = [];
+
+for (var i = 0; i < dares.length; i++) {
+    segs.push({
+        text = i + 1,
+        fillStyle = colors[i]
+    });
+}
+
 let wheel = new Winwheel({
     'numSegments': 4,
     'textOrientation': 'vertical',
@@ -16,7 +28,37 @@ let wheel = new Winwheel({
     }],
     'animation': {
         'type': 'spinToStop',
-        'duration': 5,
-        'spins': 8
+        'duration': 2,
+        'spins': 3,
+        'callbackFinished': onSpinFinished
+    },
+    'pointerGuide': {
+        'display': true,
+        'strokeStyle': 'red',
+        'lineWidth': 3
     }
 });
+
+var canvas = e('canvas');
+var label = e('label');
+
+function startSpin() {
+    // Stop any current animation.
+    wheel.stopAnimation(false);
+
+    // Reset the rotation angle to less than or equal to 360 so spinning again works as expected.
+    // Setting to modulus (%) 360 keeps the current position.
+    wheel.rotationAngle = wheel.rotationAngle % 360;
+
+    // Start animation.
+    wheel.startAnimation();
+}
+
+function onSpinFinished(seg) {
+    var seg_num = wheel.getIndicatedSegmentNumber();
+    label.innerText = seg_num;
+    prompt(seg.text);
+    wheel.deleteSegment(seg_num);
+    wheel.draw();
+}
+canvas.onclick = startSpin;
