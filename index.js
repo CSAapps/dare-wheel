b5096388fb2ae583ede5591126de2380e8a5b6ad
CSA <abeywickramakcs.19@uom.lk>
2021-06-16 18:55:24 +0530

    add pupup

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ for (var i = 0; i < dares.length; i++) {
 
 let wheel = new Winwheel({
     'textAlignment': 'outer',
+    'textMargin': 30,
+    'textFillStyle': '#ffffff',
+    'textFontFamily': 'Comic Sans MS',
+    'textFontSize': 30,
+    'strokeStyle': '#ffffff',
     'pointerAngle': 90,
     'numSegments': segs.length,
     'segments': segs,
@@ -50,4 +55,30 @@ function onSpinFinished(seg) {
     // wheel.deleteSegment(seg_num);
     // wheel.draw();
 }
-canvas.onclick = startSpin;
+// canvas.onclick = startSpin;
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "flex";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
