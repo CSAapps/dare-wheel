37735bf14e7f0787cf7cd4bed252d57d07794bb4
CSA <abeywickramakcs.19@uom.lk>
2021-06-16 12:56:23 +0530

    add index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+let wheel = new Winwheel({
+    'numSegments': 4,
+    'segments': [{
+        'fillStyle': '#eae56f',
+        'text': 'Prize One'
+    }, {
+        'fillStyle': '#89f26e',
+        'text': 'Prize Two'
+    }, {
+        'fillStyle': '#7de6ef',
+        'text': 'Prize Three'
+    }, {
+        'fillStyle': '#e7706f',
+        'text': 'Prize Four'
+    }],
+    'animation': {
+        'type': 'spinToStop',
+        'duration': 5,
+        'spins': 8
+    }
+});
